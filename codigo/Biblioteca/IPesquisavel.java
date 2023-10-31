d8e01d2aa36dcc8a417f852086cde550c3cd22a9
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-10-31 20:44:30 -0300

    Update IPesquisavel.java

--- codigo/Biblioteca/IPesquisavel.java
+++ codigo/Biblioteca/IPesquisavel.java
@@ -1,13 +1,23 @@
 package Biblioteca;
+
 public interface IPesquisavel {
-    public void listarTodos();
-    public void listarTitulo();
-    public void listarAutor();
-    public void listarAno();
-    public void listarTipo();
-    public void listarTese();
-    public void listarRevista();
-    public void listarDVD();
-    public void listarCD();
-    public void listarLivro();
+	public void listarTodos();
+
+	public void listarTitulo();
+
+	public void listarAutor();
+
+	public void listarAno();
+
+	public void listarTipo();
+
+	public void listarTese();
+
+	public void listarRevista();
+
+	public void listarDVD();
+
+	public void listarCD();
+
+	public void listarLivro();
 }
