d3ee7a2ddc0cf532ae8dcaa29c49afbdee71113c
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-10-31 20:43:43 -0300

    Update Biblioteca.java

--- codigo/Biblioteca/Biblioteca.java
+++ codigo/Biblioteca/Biblioteca.java
@@ -1,4 +1,5 @@
 package Biblioteca;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -6,185 +7,203 @@ import java.util.List;
 
 public class Biblioteca implements IPesquisavel {
 
-    List<Tese> teses = new ArrayList<>();
-    List<Revista> revistas = new ArrayList<>();
-    List<DVD> DVDs = new ArrayList<>();
-    List<CD> CDs = new ArrayList<>();
-    List<Livro> livros = new ArrayList<>();
-    List<Item> Items = new ArrayList<>();
+	List<Tese> teses = new ArrayList<>();
+	List<Revista> revistas = new ArrayList<>();
+	List<DVD> DVDs = new ArrayList<>();
+	List<CD> CDs = new ArrayList<>();
+	List<Livro> livros = new ArrayList<>();
+	List<Item> Items = new ArrayList<>();
 
-    //Cadastrar Teses na Bibilioteca
-    public void addTeses(int id, String Title, String Author, int Year, int Quantity){
-        teses.add(new Tese(id, Title, Author, Year, Quantity));
-        System.out.println("Tese cadastrada com sucesso!");
-    }
+	// Cadastrar Teses na Bibilioteca
+	public void addTeses(int id, String Title, String Author, int Year, int Quantity) {
+		getTeses().add(new Tese(id, Title, Author, Year, Quantity));
+		System.out.println("\nTese cadastrada com sucesso!");
+	}
 
-    //Cadastrar Revistas na Biblioteca
-    public void addRevista(int id, String Title, String Author, int Year, int Quantity){
-        revistas.add(new Revista(id, Title, Author, Year, Quantity));
-        System.out.println("Revista cadastrada com sucesso");
-    }
+	// Cadastrar Revistas na Biblioteca
+	public void addRevista(int id, String Title, String Author, int Year, int Quantity) {
+		getRevistas().add(new Revista(id, Title, Author, Year, Quantity));
+		System.out.println("\nRevista cadastrada com sucesso");
+	}
 
-    //Cadastrar DVDs na Biblioteca
-    public void addDVD(int id, String Title, String Author, int Year, int Quantity){
-        DVDs.add(new DVD(id, Title, Author, Year, Quantity, 0));
-        System.out.println("DVD cadastrado com sucesso");
-    }
+	// Cadastrar DVDs na Biblioteca
+	public void addDVD(int id, String Title, String Author, int Year, int Quantity) {
+		getDVDs().add(new DVD(id, Title, Author, Year, Quantity, 0));
+		System.out.println("\nDVD cadastrado com sucesso");
+	}
 
-    //Cadastrar CDs na Biblioteca
-    public void addCD(int id, String Title, String Author, int Year, int Quantity){
-        CDs.add(new CD(id, Title, Author, Year, Quantity, 0));
-        System.out.println("DVD cadastrado com sucesso");
-    }
+	// Cadastrar CDs na Biblioteca
+	public void addCD(int id, String Title, String Author, int Year, int Quantity) {
+		getCDs().add(new CD(id, Title, Author, Year, Quantity, 0));
+		System.out.println("\nDVD cadastrado com sucesso");
+	}
 
-    //Cadastrar Livros na Biblioteca
-    public void addLivro(int id, String Titulo, String Author, int Year, int Quantity){
-        livros.add(new Livro(id, Titulo, Author, Year, Quantity, 0));
-        System.out.println("Livro Cadastrado com Sucesso!");
-    }
+	// Cadastrar Livros na Biblioteca
+	public void addLivro(int id, String Titulo, String Author, int Year, int Quantity) {
+		getLivros().add(new Livro(id, Titulo, Author, Year, Quantity, 0));
+		System.out.println("\nLivro Cadastrado com Sucesso!");
+	}
 
-    public Biblioteca(){}
+	public Biblioteca() {
+	}
 
-    public void listarTodos(){
-        List<Item> Items = new ArrayList<>();
+	public void listarTodos() {
+		List<Item> Items = new ArrayList<>();
 
-        Items = addTodos();
+		Items = addTodos();
 
-        for(int i = 0; i < Items.size(); i++){
-            Items.get(i).printItem();
-        }
-    }
+		for (int i = 0; i < Items.size(); i++) {
+			Items.get(i).printItem();
+		}
+	}
 
+	// Ordenar os Items pelo titulo
+	public void listarTitulo() {
+		List<Item> Items = new ArrayList<>();
+		Items = addTodos();
+		Collections.sort(Items, new Comparator<Item>() {
+			@Override
+			public int compare(Item o1, Item o2) {
+				return o1.titulo.compareTo(o2.titulo);
+			}
+		});
 
-    //Ordenar os Items pelo titulo
-    public void listarTitulo(){
-        List<Item> Items = new ArrayList<>();
+		for (int i = 0; i < Items.size(); i++) {
+			Items.get(i).printItem();
+		}
+	}
 
-        Items = addTodos();
+	// Ordenar os Items pelo Autor
 
-        Collections.sort(Items, new Comparator<Item>() {
-            @Override
-            public int compare(Item o1, Item o2) {
-                return o1.titulo.compareTo(o2.titulo);
-            }
-        });
+	public void listarAutor() {
+		List<Item> Items = new ArrayList<>();
 
-        for(int i = 0; i < Items.size(); i++){
-            Items.get(i).printItem();
-        }
-    }   
+		Items = addTodos();
 
-    //Ordenar os Items pelo Autor
+		Collections.sort(Items, new Comparator<Item>() {
+			@Override
+			public int compare(Item o1, Item o2) {
+				return o1.autor.compareTo(o2.autor);
+			}
+		});
 
-    public void listarAutor(){
-        List<Item> Items = new ArrayList<>();
+		for (int i = 0; i < Items.size(); i++) {
+			Items.get(i).printItem();
+		}
+	}
 
-        Items = addTodos();
+	// Ordenar os Items pelo Ano
 
-        Collections.sort(Items, new Comparator<Item>() {
-            @Override
-            public int compare(Item o1, Item o2) {
-                return o1.autor.compareTo(o2.autor);
-            }
-        });
+	public void listarAno() {
+		List<Item> Items = new ArrayList<>();
 
-        for(int i = 0; i < Items.size(); i++){
-            Items.get(i).printItem();
-        }
-    }  
+		Items = addTodos();
 
-    //Ordenar os Items pelo Ano
+		Collections.sort(Items, new Comparator<Item>() {
+			@Override
+			public int compare(Item o1, Item o2) {
+				return Integer.valueOf(o1.ano).compareTo(o2.ano);
+			}
+		});
 
-    public void listarAno(){
-        List<Item> Items = new ArrayList<>();
+		for (int i = 0; i < Items.size(); i++) {
+			Items.get(i).printItem();
+		}
+	}
 
-        Items = addTodos();
+	// Ordenar os Items pelo Tipo
+	public void listarTipo() {
+		List<Item> Items = new ArrayList<>();
 
-        Collections.sort(Items, new Comparator<Item>() {
-            @Override
-            public int compare(Item o1, Item o2) {
-                return Integer.valueOf(o1.ano).compareTo(o2.ano);
-            }
-        });
+		Items = addTodos();
 
-        for(int i = 0; i < Items.size(); i++){
-            Items.get(i).printItem();
-        }
-    }  
+		Collections.sort(Items, new Comparator<Item>() {
+			@Override
+			public int compare(Item o1, Item o2) {
+				return o1.tipo.compareTo(o2.tipo);
+			}
+		});
 
+		for (int i = 0; i < Items.size(); i++) {
+			Items.get(i).printItem();
+		}
+	}
 
-    //Ordenar os Items pelo Tipo
-    public void listarTipo(){
-        List<Item> Items = new ArrayList<>();
+	// Listar somente Teses
+	public void listarTese() {
+		for (int i = 0; i < getTeses().size(); i++) {
+			getTeses().get(i).printItem();
+		}
+	}
 
-        Items = addTodos();
+	// Listar somente Revistas
+	public void listarRevista() {
+		for (int i = 0; i < getRevistas().size(); i++) {
+			getRevistas().get(i).printItem();
+		}
+	}
 
-        Collections.sort(Items, new Comparator<Item>() {
-            @Override
-            public int compare(Item o1, Item o2) {
-                return o1.tipo.compareTo(o2.tipo);
-            }
-        });
+	// Listar somente DVDs
+	public void listarDVD() {
+		for (int i = 0; i < getDVDs().size(); i++) {
+			getDVDs().get(i).printItem();
+		}
+	}
 
-        for(int i = 0; i < Items.size(); i++){
-            Items.get(i).printItem();
-        }
-    }  
+	// Listar somente CDs
+	public void listarCD() {
+		for (int i = 0; i < getCDs().size(); i++) {
+			getCDs().get(i).printItem();
+		}
+	}
 
-    //Listar somente Teses
-    public void listarTese(){
-        for(int i = 0; i < teses.size(); i++){
-            teses.get(i).printItem();
-        }
-    }
+	// Listar somente Livros
+	public void listarLivro() {
+		for (int i = 0; i < getLivros().size(); i++) {
+			getLivros().get(i).printItem();
+		}
+	}
 
-    //Listar somente Revistas
-    public void listarRevista(){
-        for(int i = 0; i < revistas.size(); i++){
-            revistas.get(i).printItem();
-        }
-    }
+	public List<Item> addTodos() {
+		List<Item> Items = new ArrayList<>();
 
-    //Listar somente DVDs
-    public void listarDVD(){
-        for(int i = 0; i < DVDs.size(); i++){
-            DVDs.get(i).printItem();
-        }
-    }
+		for (int i = 0; i < getTeses().size(); i++) {
+			Items.add(getTeses().get(i));
+		}
+		for (int i = 0; i < getRevistas().size(); i++) {
+			Items.add(getRevistas().get(i));
+		}
+		for (int i = 0; i < getDVDs().size(); i++) {
+			Items.add(getDVDs().get(i));
+		}
+		for (int i = 0; i < getCDs().size(); i++) {
+			Items.add(getCDs().get(i));
+		}
+		for (int i = 0; i < getLivros().size(); i++) {
+			Items.add(getLivros().get(i));
+		}
 
-    //Listar somente CDs
-    public void listarCD(){
-        for(int i = 0; i < CDs.size(); i++){
-            CDs.get(i).printItem();
-        }
-    }
+		return Items;
+	}
 
-    //Listar somente Livros
-    public void listarLivro(){
-        for(int i = 0; i < livros.size(); i++){
-            livros.get(i).printItem();
-        }
-    }    
+	public List<Tese> getTeses() {
+		return teses;
+	}
 
-    public List<Item> addTodos(){
-        List<Item> Items = new ArrayList<>();
+	public List<Revista> getRevistas() {
+		return revistas;
+	}
 
-        for(int i = 0; i < teses.size(); i++){
-            Items.add(teses.get(i));
-        }
-        for(int i = 0; i < revistas.size(); i++){
-            Items.add(revistas.get(i));
-        }
-        for(int i = 0; i < DVDs.size(); i++){
-            Items.add(DVDs.get(i));
-        }
-        for(int i = 0; i < CDs.size(); i++){
-            Items.add(CDs.get(i));
-        }
-        for(int i = 0; i < livros.size(); i++){
-            Items.add(livros.get(i));
-        }
+	public List<DVD> getDVDs() {
+		return DVDs;
+	}
 
-        return Items;
-    }
+	public List<CD> getCDs() {
+		return CDs;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
 }
+
