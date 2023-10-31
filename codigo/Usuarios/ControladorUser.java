f3441af7a3850637d6afa4e84606843ec480d5f7
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-10-31 20:46:17 -0300

    Update ControladorUser.java

--- codigo/Usuarios/ControladorUser.java
+++ codigo/Usuarios/ControladorUser.java
@@ -1,68 +1,69 @@
 package Usuarios;
+
 import java.util.*;
 
 public class ControladorUser {
-    List<Usuario> users;
+	List<Usuario> users;
 
-    public ControladorUser(){
-        users = new ArrayList<>();
-    }
+	public ControladorUser() {
+		users = new ArrayList<>();
+	}
 
-    public void cadastrarUsuario(int id, String nome){
-        Usuario user = new Usuario(id, nome, 0, null);
-        users.add(user);
-    }
+	public void cadastrarUsuario(int id, String nome) {
+		Usuario user = new Usuario(id, nome, 0, null);
+		users.add(user);
+	}
 
-    public void cadastrarUser(int id){
-        Scanner in = new Scanner(System.in);
+	public void cadastrarUser(int id) {
+		Scanner in = new Scanner(System.in);
 
-        System.out.println("Qual o nome do usuario?");
-        String nome = in.nextLine();
+		System.out.println("\nQual o nome do usuário?");
+		String nome = in.nextLine();
+		System.out.println("Usuário Cadastrado com sucesso!\n");
+		cadastrarUsuario(id, nome);
+	}
 
-        cadastrarUsuario(id, nome);
-    }
+	public void removeUser() {
+		Scanner in = new Scanner(System.in);
+		listarUsers();
+		System.out.println("\nQual o id do usuario?");
+		int id = in.nextInt();
+		id--;
 
-    public void removeUser(){
-        Scanner in = new Scanner(System.in);
-        listarUsers();
-        System.out.println("Qual o id do usuario?");
-        int id = in.nextInt();
-        id--;
+		Usuario user = new Usuario();
 
-        Usuario user = new Usuario();
+		users.set(id, user);
+	}
 
-        users.set(id, user);
-    }
+	public void listarUsers() {
+		for (int i = 0; i < users.size(); i++) {
+			users.get(i).listarUsuario();
+		}
+	}
 
-    public void listarUsers(){
-        for(int i = 0; i < users.size(); i++){
-            users.get(i).listarUsuario();
-        }
-    }
+	public void atualizarUser() {
+		Scanner in = new Scanner(System.in);
+		listarUsers();
+		System.out.println("\nQual o id do usuario que deseja atualizar?");
+		int id = in.nextInt();
+		id--;
+		try {
+			if (id < 0 || id > users.size()) {
+				System.out.println("Usuario não encontrado");
+			} else {
+				Usuario user = new Usuario();
+				user = users.get(id);
+				Scanner Nome = new Scanner(System.in);
 
-    public void atualizarUser(){
-        Scanner in = new Scanner(System.in);
-        listarUsers();
-        System.out.println("Qual o id do usuario que deseja atualizar?");
-        int id = in.nextInt();
-        id--;
-        try{
-            if(id < 0 || id > users.size()){
-                System.out.println("Usuario não encontrado");
-            }else{
-                Usuario user = new Usuario();
-                user = users.get(id);
-                Scanner Nome = new Scanner(System.in);
+				System.out.println("\nQual o novo nome do usuario?");
+				String nome = Nome.nextLine();
 
-                System.out.println("Qual o novo nome do usuario?");
-                String nome = Nome.nextLine();
+				user = user.atualizarUsuario(user.getID(), nome, user.getContadorItens(), user.getItensEmprestados());
 
-                user = user.atualizarUsuario(user.getID(), nome, user.getContadorItens(), user.getItensEmprestados());
-
-                users.set(id, user);
-            }
-        }catch(Exception e){
-            System.out.println("Usuario não encontrado");
-        }
-    }
+				users.set(id, user);
+			}
+		} catch (Exception e) {
+			System.out.println("Usuario não encontrado");
+		}
+	}
 }
