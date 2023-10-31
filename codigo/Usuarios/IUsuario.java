ee0d83c19c792dc90dbba4c4901647dba1fe5fc7
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-10-31 20:47:10 -0300

    Update IUsuario.java

--- codigo/Usuarios/IUsuario.java
+++ codigo/Usuarios/IUsuario.java
@@ -2,91 +2,24 @@ package Usuarios;
 
 import Biblioteca.Item;
 
-public class Usuario implements IUsuario {
-	private int ID;
-	private String nome;
-	private int contadorItens;
-	private Item[] itensEmprestados;
+public interface IUsuario {
+	public void cadastrarUsuario(int ID, String nome, int contadorItens);
 
-	public Usuario() {
-		this.ID = 0;
-		this.nome = "";
-		this.contadorItens = 0;
-		this.itensEmprestados = null;
-	}
+	public Usuario removerUsuario(Usuario usuario);
 
-	// Getters
-	public int getID() {
-		return this.ID;
-	}
+	public Usuario atualizarUsuario(int ID, String nome, int contadorItens, Item[] itensEmprestados);
 
-	public String getNome() {
-		return this.nome;
-	}
+	public void listarUsuario();
 
-	public int getContadorItens() {
-		return this.contadorItens;
-	}
+	public void pegarEmprestado(int ID, Item item);
 
-	public Item[] getItensEmprestados() {
-		return this.itensEmprestados;
-	}
+	public void devolver(int ID, Item item);
 
-	public Usuario(int ID, String nome, int contadorItens, Item[] itensEmprestados) {
-		this.ID = ID;
-		this.nome = nome;
-		this.contadorItens = contadorItens;
-		this.itensEmprestados = itensEmprestados;
-	}
+	public int getID();
 
-	public void cadastrarUsuario(int ID, String nome, int contadorItens) {
-		this.ID = ID;
-		this.nome = nome;
-		this.contadorItens = contadorItens;
-		this.itensEmprestados = null;
-	}
+	public String getNome();
 
-	public Usuario removerUsuario(Usuario usuario) {
-		usuario.ID = 0;
-		usuario.nome = "";
-		usuario.contadorItens = 0;
-		usuario.itensEmprestados = null;
+	public int getContadorItens();
 
-		return usuario;
-	}
-
-	public Usuario atualizarUsuario(int ID, String nome, int contadorItens, Item[] itensEmprestados) {
-		this.ID = ID;
-		this.nome = nome;
-		this.contadorItens = contadorItens;
-		this.itensEmprestados = itensEmprestados;
-
-		return this;
-	}
-
-	public void listarUsuario() {
-		System.out.println("\nID: " + this.ID + ", Nome: " + this.nome + ", Quantidade de itens emprestados: " + this.contadorItens);
-		System.out.println("Itens Emprestados: ");
-		if (this.itensEmprestados == null) {
-			System.out.println("Nenhum item emprestado");
-		} else {
-			for (int i = 0; i < (this.itensEmprestados).length; i++) {
-				this.itensEmprestados[i].printItem();
-			}
-		}
-	}
-
-	public void pegarEmprestado(int ID, Item item) {
-		this.itensEmprestados[this.contadorItens] = item;
-		this.contadorItens++;
-	}
-
-	public void devolver(int ID, Item item) {
-		for (int i = 0; i < (this.itensEmprestados).length; i++) {
-			if (this.itensEmprestados[i].getID() == item.getID()) {
-				this.itensEmprestados[i] = null;
-				this.contadorItens--;
-			}
-		}
-	}
+	public Item[] getItensEmprestados();
 }
