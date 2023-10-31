29b984094b98336c3b22d933c938708863645de4
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-10-31 20:45:06 -0300

    Update Livro.java

--- codigo/Biblioteca/Livro.java
+++ codigo/Biblioteca/Livro.java
@@ -1,9 +1,10 @@
 package Biblioteca;
-public class Livro extends Item {
-    int vezesEmprestado;
 
-    public Livro(int id, String Title, String Author, int Year, int Quantity, int Emprestado){
-        super(id, Title, Author, Year, "Livro", Quantity);
-        this.vezesEmprestado = Emprestado;
-    }
+public class Livro extends Item {
+	int vezesEmprestado;
+
+	public Livro(int id, String Title, String Author, int Year, int Quantity, int Emprestado) {
+		super(id, Title, Author, Year, "Livro", Quantity);
+		this.vezesEmprestado = Emprestado;
+	}
 }
