19d9d17707565944b1015afa793feb9611aab06d
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-11-07 20:32:10 -0300

    Update ControladorUser.java

--- codigo/Usuarios/ControladorUser.java
+++ codigo/Usuarios/ControladorUser.java
@@ -1,6 +1,11 @@
 package Usuarios;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import Biblioteca.Item;
 
 public class ControladorUser {
 	List<Usuario> users;
@@ -10,7 +15,7 @@ public class ControladorUser {
 	}
 
 	public void cadastrarUsuario(int id, String nome) {
-		Usuario user = new Usuario(id, nome, 0, null);
+		Usuario user = new Usuario(id, nome);
 		users.add(user);
 	}
 
@@ -25,14 +30,42 @@ public class ControladorUser {
 
 	public void removeUser() {
 		Scanner in = new Scanner(System.in);
+		boolean itemRemovido = false;
 		listarUsers();
-		System.out.println("\nQual o id do usuario?");
-		int id = in.nextInt();
-		id--;
+		int id;
+		if (users != null) {
+			if (users.isEmpty()) {
+				System.out.println("Não há itens cadastrados neste tipo.");
+				return;
+			}
+			
+			do {
+				System.out.println("\nQual o id do usuario?");
+				id = in.nextInt();
+			} while (vaidaId(id));
+			
+			int op = id;
+			Usuario userParaRemover = users.stream().filter(item -> item.getID() == op).findFirst().orElse(null);
+			Usuario user = new Usuario();
+			
+			if (userParaRemover != null) {
+				userParaRemover.printItem();
+				System.out.println("Tem certeza de que deseja excluir este item? (sim/nao)");
+				String resposta = in.nextLine();
 
-		Usuario user = new Usuario();
+				if (resposta.equalsIgnoreCase("sim")) {
+					users.remove(userParaRemover);
+					System.out.println("Item excluído com sucesso.");
+					itemRemovido = true;
+				} else {
+					System.out.println("Ação de exclusão cancelada.");
+				}
+			} else {
+				System.out.println("ID não encontrado.");
+			}
 
-		users.set(id, user);
+			users.set(id, user);
+		}
 	}
 
 	public void listarUsers() {
@@ -47,39 +80,45 @@ public class ControladorUser {
 		System.out.println("\nQual o id do usuario que deseja atualizar?");
 		int id = in.nextInt();
 		id--;
-		try {
-			if (id < 0 || id > users.size()) {
-				System.out.println("Usuario não encontrado");
-			} else {
-				Usuario user = new Usuario();
-				user = users.get(id);
-				Scanner Nome = new Scanner(System.in);
 
-				System.out.println("\nQual o novo nome do usuario?");
-				String nome = Nome.nextLine();
-
-				user = user.atualizarUsuario(user.getID(), nome, user.getContadorItens(), user.getItensEmprestados());
-
-				users.set(id, user);
-			}
-		} catch (Exception e) {
+		if (id < 0 || id >= users.size()) {
 			System.out.println("Usuario não encontrado");
+			return;
 		}
+
+		Usuario user = users.get(id);
+		System.out.println("\nQual o novo nome do usuario?");
+		in.nextLine();
+		String nome = in.nextLine();
+
+		user.atualizarUsuario(user.getID(), nome, Arrays.asList(user.getItensEmprestados()));
+		users.set(id, user);
 	}
 
 	public boolean vaidaId(int idUser) {
-		for(Usuario a : users) {
-			if(a.getID() == idUser) {
+		for (Usuario a : users) {
+			if (a.getID() == idUser) {
 				return true;
 			}
 		}
 		return false;
 	}
 
+	public String getNomeByID(int id) {
+		for (Usuario a : users) {
+			if (a.getID() == id) {
+				return a.getNome();
+			}
+		}
+		return null;
+	}
+
 	public boolean podePegarEmprestado(int idUser) {
-		for(Usuario a : users) {
-			if(!(a.getContadorItens() >= 3)) {
-				return true;
+		for (Usuario a : users) {
+			if (a.getID() == idUser) {
+				if (a.getContadorItens() < 3) {
+					return true;
+				}
 			}
 		}
 		return false;
